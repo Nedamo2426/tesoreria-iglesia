b837683d380de22baec2dbacc507c8259a2cd6e2
Nedamo2426 <arnaldo.uribej@gmail.com>
2025-05-09 11:34:40 -0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TesoreriaApp from './TesoreriaApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TesoreriaApp />
+  </React.StrictMode>
+);
