0e94562b68dcbba746bb5ff3833b597033d7ed85
Nedamo2426 <arnaldo.uribej@gmail.com>
2025-05-09 11:33:55 -0400

    Delete prog directory

--- prog/src/components/ui/card.jsx
+++
@@ -1,7 +0,0 @@
-export function Card({ children, className = '' }) {
-  return <div className={`bg-white shadow rounded-2xl p-2 ${className}`}>{children}</div>;
-}
-
-export function CardContent({ children, className = '' }) {
-  return <div className={`p-2 ${className}`}>{children}</div>;
-}
--- prog/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import TesoreriaApp from './TesoreriaApp';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TesoreriaApp />
-  </React.StrictMode>
-);
